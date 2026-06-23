1e2e9de6ee25550f51b8f6b11f60f69b69546c11
SoyidWahyuDanamon <soyidvanced@gmail.com>
2026-06-23 23:50:23 +0700

    adding multi

---
+++ hello.js
@@ -0,0 +1,9 @@
+function tambah(a, b) {
+    return a + b;
+}
+
+const angka1 = 5;
+const angka2 = 10;
+const hasil = tambah(angka1, angka2);
+
+console.log(`Hasil penjumlahan dari ${angka1} + ${angka2} adalah: ${hasil}`);
